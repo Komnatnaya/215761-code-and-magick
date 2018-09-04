6481e4c14ff65bace2b372bd121c92f7ead4fcef
Андрей <39153320+samsam4055@users.noreply.github.com>
2018-09-04 17:12:18 +0300

    тест работы на странице демки

--- js/stat.js
+++ js/stat.js
@@ -25,3 +25,65 @@ var renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'blue';
   ctx.fillRect(320, 100, 40, times[3] / 40);
 };  // <- тест логики работы
+
+
+// работа на странице сайта ->>
+
+//
+//
+// var CLOUD_WIDTH = 500;
+// var CLOUD_HEIGHT = 200;
+// var CLOUD_X = 100;
+// var CLOUD_Y = 50;
+// var GAP = 10;
+// var FONT_GAP = 15;
+// var TEXT_WIDTH = 50;
+// var BAR_HEIGHT = 20;
+// var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+//
+// var renderCloud = function(ctx, x, y, color) {
+//   ctx.fillStyle = color;
+//   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+// };
+//
+// var getMaxElement = function(arr) {
+//   var maxElement = arr[0];
+//
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] > maxElement) {
+//       maxElement = arr[i];
+//     }
+//   }
+//
+//   return maxElement;
+// };
+//
+// window.renderStatistics = function(ctx, players, times) {
+//   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+//   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+//
+// function getRandomArbitary(min, max)
+// {
+//   return Math.random() * (max - min) + min;
+// };
+//
+//
+//
+//
+//  var getRandomColor = function ()
+// {
+//   return 'rgba(0, 0, 255, ' + getRandomArbitary(0.1, 1) + ')';
+// };
+//
+//
+//   var maxTime = getMaxElement(times);
+//
+//   for (var i = 0; i < players.length; i++) {
+//    ctx.fillStyle = 'black';
+//     ctx.fillText(players[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i);
+//     if (players[i]=='Вы') {ctx.fillStyle = 'red';} else {ctx.fillStyle = getRandomColor();}
+//
+//     ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
+//   }
+// };
+//
