b2e6191117970028107331ecf1ca45ea054ef8e6
Андрей <39153320+samsam4055@users.noreply.github.com>
2018-09-04 14:40:27 +0300

    подключеие stat.js

--- js/stat.js
+++ js/stat.js
@@ -11,17 +11,17 @@ var renderStatistics = function (ctx, names, times) {
 
   ctx.strokeText(names[0], 140, 80);
   ctx.fillStyle = 'blue';
-  ctx.fillRect(140, 100, 40, times[0] / 30);
+  ctx.fillRect(140, 100, 40, times[0] / 40);
 
   ctx.strokeText(names[1], 200, 80);
   ctx.fillStyle = 'blue';
-  ctx.fillRect(200, 100, 40, times[1] / 30);
+  ctx.fillRect(200, 100, 40, times[1] / 40);
 
   ctx.strokeText(names[2], 260, 80);
   ctx.fillStyle = 'blue';
-  ctx.fillRect(260, 100, 40, times[2] / 30);
+  ctx.fillRect(260, 100, 40, times[2] / 40);
 
   ctx.strokeText(names[3], 320, 80);
   ctx.fillStyle = 'blue';
-  ctx.fillRect(320, 100, 40, times[3] / 30);
+  ctx.fillRect(320, 100, 40, times[3] / 40);
 };  // <- тест логики работы
