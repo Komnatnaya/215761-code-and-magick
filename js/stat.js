c1566e1415d1c34b6a416ab6c4c84bdb3a41c224
Андрей <39153320+samsam4055@users.noreply.github.com>
2018-09-04 13:45:30 +0300

    тест логики работы скрипта

---
+++ js/stat.js
@@ -0,0 +1,27 @@
+var renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillStyle = 'rgb(255,255,255)';
+  ctx.fillRect(100, 10, 420, 270);
+
+
+  ctx.font = '16px PT Mono';
+  ctx.strokeText('Ура вы победили!', 140, 40);
+  ctx.strokeText('Список результатов:', 140, 60);
+
+  ctx.strokeText(names[0], 140, 80);
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(140, 100, 40, times[0] / 30);
+
+  ctx.strokeText(names[1], 200, 80);
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(200, 100, 40, times[1] / 30);
+
+  ctx.strokeText(names[2], 260, 80);
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(260, 100, 40, times[2] / 30);
+
+  ctx.strokeText(names[3], 320, 80);
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(320, 100, 40, times[3] / 30);
+};  // <- тест логики работы
